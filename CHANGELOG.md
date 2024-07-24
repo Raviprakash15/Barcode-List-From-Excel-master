c15ea3e9aa5a7d25fad0518ca795fd97dc623b65
ravi <ravi@keabis.com>
2024-07-24 11:38:12 +0530

    initialcommit

---
+++ CHANGELOG.md
@@ -0,0 +1,51 @@
+# CHANGELOG
+
+## v2.0.2 (2024-04-30)
+
+* Update SheetJS to v0.20.2 xlsx.full.min.js  https://cdn.sheetjs.com/
+* Update JsBarcode to v3.11.6
+* Update Bootstrap to v5.3.3
+* Update Bootstrap icons to v1.11.3
+
+## v2.0.1 (2023-05-05) 
+
+* Update SheetJS to v0.19.3 xlsx.full.min.js
+
+## v2.0.0 (2023-04-04)
+
+* Add Boostrap Icons 
+* Update Bootstrap to v5.3.0-alpha3
+* Update JsBarcode to v3.11.5
+* Update SheetJS to v0.19.2 xlsx.full.min.js
+* Remove jQuery
+* Remove jsZip
+
+## v1.0.5 (2021-05-09)
+
+* Add an online demo to GitHub pages
+  https://kyriakosgian.github.io/Barcode-List-From-Excel/
+
+## v1.0.4 (2021-01-20)
+
+* Update SheetJS to v0.16.9
+* Update JsBarcode to v3.11.3
+* Update Bootstrap to v5.0.0-beta1
+
+## v1.0.3 (2020-05-03)
+
+* Clear some code
+* Update JsBarcode to v3.11.0
+* Add SetBarcodeHeight function & functionality
+
+## v1.0.2 (2020-04-25)
+
+* Add an online demo to Netlify
+ https://barcodelistfromexcel.netlify.app/
+
+## v1.0.1 (2020-03-9)
+
+* Fix print page fit to A4
+
+## v1.0.0 (2020-02-17)
+
+* BarcodeListFromExcel Project Deployment
---
+++ assets/js/main.js
@@ -0,0 +1,125 @@
+//Name the list title 
+const typeHandler = function (e) {
+    document.getElementById('printTitle').innerHTML = e.target.value;
+    localStorage.setItem('Title', e.target.value); //Save Title to localStorage
+}
+
+document.getElementById('inputTitle').addEventListener('input', typeHandler);
+document.getElementById('inputTitle').addEventListener('propertychange', typeHandler);
+
+//Load Title from localStorage if exist
+if (localStorage.getItem("Title") != null) {
+    document.getElementById('printTitle').innerHTML = localStorage.getItem("Title");
+    document.getElementById('inputTitle').defaultValue = localStorage.getItem("Title");
+}
+
+//Add date to bottom
+// document.getElementById('date').innerHTML = Date();
+
+//Excel barcode functions
+const uploadedFile = document.getElementById('uploadedFile');
+const excelDataTable = document.getElementById('excelDataTable');
+
+uploadedFile.addEventListener('change', handleFileSelect, false);
+
+function handleFileSelect(evt) {
+    excelDataTable.innerHTML = "";
+    const files = evt.target.files;
+    console.log(files[0])
+    const xl2json = new ExcelToJSON();
+    xl2json.parseExcel(files[0]);
+}
+
+class ExcelToJSON {
+    parseExcel(file) {
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            const data = e.target.result;
+            const workbook = XLSX.read(data, { type: 'binary' });
+
+            workbook.SheetNames.forEach((sheetName) => {
+                const XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+                const json_object = JSON.stringify(XL_row_object);
+               // console.log(json_object)
+
+                buildHtmlTable(JSON.parse(json_object), 'excelDataTable');
+
+                const targetTDs = excelDataTable.querySelectorAll('tr > td:last-child');
+
+                for (let i = 0; i < targetTDs.length; i++) {
+                    const td = targetTDs[i];
+                    td.innerHTML = `<svg class="barcode" jsbarcode-value="${td.innerHTML}" jsbarcode-height="35" jsbarcode-width="2"</svg>`;
+                    td.style.textAlign= "start"
+                }
+
+                JsBarcode(".barcode").init();
+            });
+        };
+
+        reader.onerror = (ex) => console.log(ex);
+
+        reader.readAsArrayBuffer(file);
+    }
+}
+
+// Builds the HTML Table out of myList.
+function buildHtmlTable(myList, selector) {
+    const columns = addAllColumnHeaders(myList, selector);
+   // console.log(columns)
+
+    const table = document.getElementById(selector);
+   
+
+    for (let i = 0; i < myList.length; i++) {
+        const row = table.insertRow(-1);
+        console.log(row)
+        for (let colIndex = 0; colIndex < columns.length; colIndex++) {
+            let cellValue = myList[i][columns[colIndex]];
+
+            if (colIndex === 0) {
+                // do nothing
+            }
+            if (cellValue == null) {
+                cellValue = "";
+            }
+            const cell = row.insertCell(-1);
+            cell.innerHTML = cellValue;
+            cell.style.textAlign = "start";
+           
+        }
+    }
+}
+
+// Adds a header row to the table and returns the set of columns.
+// Need to do union of keys from all records as some records may not contain
+// all records.
+function addAllColumnHeaders(myList, selector) {
+    const columnSet = [];
+    const headerTr = document.createElement('tr');
+
+    for (let i = 0; i < myList.length; i++) {
+        const rowHash = myList[i];
+        for (const key in rowHash) {
+            if (!columnSet.includes(key)) {
+                columnSet.push(key);
+               const th = document.createElement('th');
+               th.textContent = key;
+               th.style.textAlign="start"
+               headerTr.appendChild(th);
+               headerTr.style.textAlign="start"
+            }
+        }
+    }
+    document.getElementById(selector).appendChild(headerTr);
+    return columnSet;
+}
+
+function SetBarcodeHeight(height) {
+    const elements = document.querySelectorAll(".barcode");
+    for (let i = 0; i < elements.length; i++) {
+        const currentElements = elements[i];
+        currentElements.setAttribute('jsbarcode-height', height);
+    }
+    JsBarcode(".barcode").init();
+}
